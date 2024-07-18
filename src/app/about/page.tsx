d52da49dd7f5d56d60ac74540d6516e03d10b7b9
BasitAyaz <basitahmed1997@gmail.com>
2024-07-19 01:11:03 +0500

    About Page Work

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,23 @@
 import BABox from "../components/BABox";
+import BAHeading from "../components/BAHeading";
+import BAPera from "../components/BAPera";
+import FooterSection from "../reuseable_layouts/FooterSection";
+import Navbar from "../reuseable_layouts/Navbar";
+import PageHeroSection from "../reuseable_layouts/PageHeroSection";
 
 export default function About() {
     return <>
         <BABox>
-            <h1>About</h1>
+            <Navbar />
+            <PageHeroSection text={"About Us"} />
+            <BABox className="p-10 md:p-20">
+                <BAHeading>About Us</BAHeading>
+                <BAPera>
+                    Mubashir Curtain is a renowned and well-known curtain manufacturing company.
+                    We specialize in producing high-quality, durable, and stylish curtains that meet your needs and expectations.
+                </BAPera>
+            </BABox>
+            <FooterSection />
         </BABox>
     </>
 }
